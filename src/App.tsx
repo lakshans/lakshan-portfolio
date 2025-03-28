6e832f5fb9ad1748c38d36ef8431a33b698126b2
Lakshan Sivananthan <lakshan@nextgenbt.com>
2025-03-28 19:03:25 -0400

    Fixed @/ import issue. Added light and dark theme framework. Added context cards. Cleaned up layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
 import './App.css'
-
+import { Button } from './components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from './components/ui/card';
+import { ThemeProvider } from './components/theme-provider';
+import { ModeToggle } from './components/mode-toggle';
 function App() {
   return (
+    <ThemeProvider>
     <>
-    <div className="flex bg-auto min-h-screen items-center justify-center bg-gradient-to-tr from-blue-500 to-purple-500">
-      <div className="p-10">
-        <div className="w-max">
-          <p className="typing-animation overflow-hidden whitespace-nowrap pr-1 text-5xl font-bold text-white">
-            Hi, my name is Lakshan.
-          </p>
-          <p className="p-3 font-style: italic text-white text-lg">
-            I'm an Enthusiast @    Tech | Photography | Travel
-          </p>
+
+    <div className="bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="flex justify-end p-5">
+          <ModeToggle />
+      </div>
+
+      <div className="flex min-h-screen justify-center">
+
+        <div className="flex flex-col items-center">
+          <div className="w-max">
+            <p className="typing-animation overflow-hidden whitespace-nowrap mt-70 w-80 text-2xl font-bold text-white md:text-3xl md:mt-70 lg:text-5xl lg:mt-100">
+              Hi, my name is Lakshan.
+            </p>
+          </div>
+          <div className="text-sm p-1 font-style: italic text-white md:text-md md:p-2 lg:text-lg lg:p-3">
+              I'm an Enthusiast @ Tech | Photography | Travel
+          </div>
+          <div className="w-70 grid grid-rows-3 gap-4 mt-5 md:grid-rows-3 md:w-75 md:mt-10 lg:grid-cols-3 lg:w-220">
+            <Card>
+              <CardHeader>
+                <CardTitle>Tech</CardTitle>
+                <CardDescription>Cool stuff I've worked on</CardDescription>
+              </CardHeader>
+              <CardFooter className="justify-center">
+                <Button variant="default">View</Button>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Photography</CardTitle>
+                <CardDescription>Spent way too long editing these</CardDescription>
+              </CardHeader>
+              <CardFooter className="justify-center">
+                <Button variant="default">View</Button>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Travel</CardTitle>
+                <CardDescription>Where to next?</CardDescription>
+              </CardHeader>
+              <CardFooter className="justify-center">
+                <Button variant="default">View</Button>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
     </>
+    </ThemeProvider>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-  }
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 })
